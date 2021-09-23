7e316f3050d87dc5040659fd3fd591f9d7f2e2d7
Ignat <timofeev.ignat.i20@gmail.com>
2021-09-24 00:53:15 +0300

    br 2.2

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <v-app>
-  <v-navigation-drawer app></v-navigation-drawer> 
+  <v-navigation-drawer app v-model="drawer"></v-navigation-drawer> 
   <v-app-bar app dark color="primary">
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-       <v-btn  text><v-icon left>mdi-Magnify</v-icon>Link One</v-btn> 
-       <v-btn  text>Link Two</v-btn> 
-       <v-btn  text>Link Three</v-btn>
+       <v-btn  text><v-icon left>mdi-magnify</v-icon>Поиск</v-btn> 
+       <v-btn  text><v-icon left>mdi-chat</v-icon>Чаты</v-btn>
+       <v-btn  text><v-icon left>mdi-bell</v-icon>Уведомления</v-btn>
+       <v-btn  text><v-icon left>mdi-map-marker</v-icon>Контакты</v-btn>
+       <v-btn  text><v-icon left>mdi-bullhorn</v-icon>Тех.поддержка</v-btn>
+       <v-btn  text><v-icon left>mdi-account</v-icon>Аккаунт</v-btn>
     </v-toolbar-items>
   </v-app-bar>
   <v-content>
@@ -18,6 +21,10 @@
 
 <script>
 export default {
-
+  data() {
+    return {
+      drawer: false
+    }
+  }
 }
 </script>
