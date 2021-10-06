7d0dc6df51f0e940f86f8e06cd9715cc16e6ae59
Ignat <timofeev.ignat.i20@gmail.com>
2021-10-07 01:47:50 +0300

    br 2.4\5

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,63 @@
 <template>
-<v-app>
-<v-navigation-drawer app v-model="drawer"></v-navigation-drawer>
-<v-app-bar app dark color="primary">
-<v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-<v-spacer></v-spacer>
-<v-toolbar-items class="hidden-sm-and-down">
-       <v-btn  text><v-icon left>mdi-magnify</v-icon>Поиск</v-btn> 
-       <v-btn  text><v-icon left>mdi-chat</v-icon>Чаты</v-btn>
-       <v-btn  text><v-icon left>mdi-bell</v-icon>Уведомления</v-btn>
-       <v-btn  text><v-icon left>mdi-map-marker</v-icon>Контакты</v-btn>
-       <v-btn  text><v-icon left>mdi-bullhorn</v-icon>Тех.поддержка</v-btn>
-       <v-btn  text><v-icon left>mdi-account</v-icon>Аккаунт</v-btn>
-</v-toolbar-items>
-</v-app-bar>
-<v-content>
-<router-view></router-view>
-</v-content>
-</v-app>
+  <v-app>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+          КИПУ
+          </v-list-item-title>
+          <v-list-item-subtitle>
+          Учебный проект
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    <v-divider></v-divider>
+    <v-list dense>
+      <v-list-item-group color="primary">
+        <v-list-item v-for="link in menu" :key="link.title">
+          <v-list-item-icon>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ link.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+    </v-navigation-drawer>
+  <v-app-bar app dark color="primary">
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn v-for="link in links" :key="link.title" text><v-icon left>{{ link.icon }}</v-icon>{{ link.title }}</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+  <v-main>
+  <!--<router-view></router-view>-->
+  </v-main>
+  </v-app>
 </template>
 <script>
-export default {
-data() {
-return {
-drawer: false
-}
-}
-}
+  export default {
+  data() {
+    return {
+      drawer: false,
+      links: [
+		{title:"Поиск", icon:"mdi-magnify", url:"/login"},
+		{title:"Чат", icon:"mdi-chat", url:"/registration"},
+		{title:"Уведомления", icon:"mdi-bell", url:"/orders"},
+		{title:"Контакты", icon:"mdi-map-marker", url:"/new"},
+		{title:"Тех.поддержка", icon:"mdi-bullhorn", url:"/list"},
+		{title:"Аккаунт", icon:"mdi-account", url:"/list"}
+	],
+
+	menu: [
+		{title:"Новости", icon:"mdi-newspaper-variant", url:"/login"},
+		{title:"Форум", icon:"mdi-account-group", url:"/registration"},
+		{title:"Сообщения", icon:"mdi-forum", url:"/new"},
+		{title:"Личный кабинет", icon:"mdi-account-check", url:"/orders"}
+      ]
+      }
+    }
+  }
 </script>
